85b3f2ae22ba9f9aeea5d8d7531913e60efb0914
Vanshaj Saxena <vs110405@outlook.com>
2024-10-06 00:41:35 +0530

    TwoSum: solution

--- TwoSum.java
+++ TwoSum.java
@@ -1,8 +1,12 @@
+// https://leetcode.com/problems/two-sum/description/
+
+import java.util.HashMap;
+
 /**
  * **Two Sum Problem**:
  *
- * Given an array of integers nums and an integer target, return indices of the
- * two numbers such that they add up to target.
+ * Given an array of integers `nums` and an integer target, return indices of
+ * the two numbers such that they add up to target.
  *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
@@ -12,6 +16,15 @@
 
 public class TwoSum {
   public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
 
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(target - nums[i])) {
+        return new int[] { i, map.get(target - nums[i]) };
+      }
+      map.put(nums[i], i);
+    }
+    // This will never be reached, the problem guarantees a solution.
+    return null;
   }
 }
