45015939e6b2955405875ac169851d4839a76633
Vanshaj Saxena <vs110405@outlook.com>
2024-10-08 00:14:39 +0530

    BestTimeToBuyAndSellStock: start at the second index

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -20,7 +20,7 @@ public class BestTimeToBuyAndSellStock {
   public int maxProfit(int[] prices) {
     int maxProfit = 0;
     int min = prices[0];
-    for (int i = 0; i < prices.length; i++) {
+    for (int i = 1; i < prices.length; i++) {
       if (prices[i] < min) {
         min = prices[i];
       } else if (maxProfit < prices[i] - min) {
