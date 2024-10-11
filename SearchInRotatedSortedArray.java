6f1bd8160124820cf8096a55a7930fd92787042c
Vanshaj Saxena <vs110405@outlook.com>
2024-10-11 17:11:33 +0530

    SearchInRotatedSortedArraySecond: solution

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -12,7 +12,7 @@
  * nums[k-1]]` (**0-indexed**). For example, `[0,1,2,4,5,6,7]` might be rotated
  * at pivot index `3` and become `[4,5,6,7,0,1,2]`.
  *
- * Given the array `nums` after the possible rotation and an integer target,
+ * Given the array `nums` after the possible rotation and an integer `target`,
  * return the index of target if it is in `nums`, or -1 if it is not in `nums`.
  *
  * You must write an algorithm with `O(log n)` runtime complexity.
