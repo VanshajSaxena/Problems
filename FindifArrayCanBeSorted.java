53b7f9164a1c73ebeaa3a0d10344cbd202d4e2df
Vanshaj Saxena <vs110405@outlook.com>
2024-11-06 18:42:00 +0530

    FindifArrayCanBeSorted: boilerplate

---
+++ FindifArrayCanBeSorted.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/find-if-array-can-be-sorted/description/
+
+/**
+ * **Find if Array Can Be Sorted**:
+ *
+ * You are given a **0-indexed** array of **positive** integers nums.
+ *
+ * In one **operation**, you can swap any two **adjacent** elements if they have
+ * the
+ * **same** number of set bits. You are allowed to do this operation **any**
+ * number of
+ * times (**including** **zero**).
+ *
+ * Return _`true` if you can sort the array, else return `false`_.
+ */
+
+public class FindifArrayCanBeSorted {
+  public boolean canSortArray(int[] nums) {
+
+  }
+}
