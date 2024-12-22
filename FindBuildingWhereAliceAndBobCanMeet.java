aabd0fc0fc7ef31988bfab0cc5bd446bc23ed021
Vanshaj Saxena <vs110405@outlook.com>
2024-12-22 23:59:51 +0530

    FindBuildingWhereAliceAndBobCanMeet: solution pending

---
+++ FindBuildingWhereAliceAndBobCanMeet.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet/description/
+
+// MARK: Pending.
+public class FindBuildingWhereAliceAndBobCanMeet {
+  public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+
+  }
+}
