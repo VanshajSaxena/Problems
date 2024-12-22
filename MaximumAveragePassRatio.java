dcfd89de9d11452ee7e28126e202bc73b0c239ce
Vanshaj Saxena <vs110405@outlook.com>
2024-12-23 00:00:06 +0530

    MaximumAveragePassRatio: solution pending

---
+++ MaximumAveragePassRatio.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/maximum-average-pass-ratio/description/
+
+/**
+ * **Maximum Average Pass Ratio**:
+ *
+ * There is a school that has classes of students and each class will be having
+ * a final exam. You are given a 2D integer array `classes`, where `classes[i] =
+ * [passi, totali]`. You know beforehand that in the `ith` class, there are
+ * `totali` total students, but only `passi` number of students will pass the
+ * exam.
+ *
+ * You are also given an integer `extraStudents`. There are another
+ * `extraStudents` brilliant students that are **guaranteed** to pass the exam
+ * of any class they are assigned to. You want to assign each of the
+ * `extraStudents` students to a class in a way that **maximizes** the
+ * **average** pass ratio across **all** the classes.
+ *
+ * The **pass ratio** of a class is equal to the number of students of the class
+ * that will pass the exam divided by the total number of students of the class.
+ * The **average pass ratio** is the sum of pass ratios of all the classes
+ * divided
+ * by the number of the classes.
+ *
+ * Return _the **maximum** possible average pass ratio after assigning the
+ * `extraStudents` students_. Answers within `10-5` of the actual answer will be
+ * accepted.
+ */
+
+// MARK: Pending.
+public class MaximumAveragePassRatio {
+  public double maxAverageRatio(int[][] classes, int extraStudents) {
+    double result = 0;
+
+    return result;
+  }
+}
