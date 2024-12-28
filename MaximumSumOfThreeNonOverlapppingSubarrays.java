9354270066e41d45858d1752cd635e6a4b5acd5d
Vanshaj Saxena <vs110405@outlook.com>
2024-12-28 23:35:58 +0530

    MaximumSumOfThreeNonOverlapppingSubarrays: solution pending

---
+++ MaximumSumOfThreeNonOverlapppingSubarrays.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/description/
+
+// MARK: Pending.
+public class MaximumSumOfThreeNonOverlapppingSubarrays {
+  public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+    return nums;
+  }
+}
