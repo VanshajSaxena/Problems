f5be6880d78544605b51d29348896ae12b57b550
Vanshaj Saxena <vs110405@outlook.com>
2025-01-22 01:16:38 +0530

    FindThePrefixCommonArrayOfTwoArrays: solution pending

---
+++ java/FindThePrefixCommonArrayOfTwoArrays.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/
+
+// MARK: Pending.
+public class FindThePrefixCommonArrayOfTwoArrays {
+  public int[] findThePrefixCommonArray(int[] A, int[] B) {
+    // HashSet<Integer> set = new HashSet<>();
+
+    for (int i = 0; i < A.length; i++) {
+
+    }
+    return new int[] {};
+  }
+}
